8df6fe47821e7f7ff9c0335cdbaa1d328efe611a
Jovanka <samudrajovanka20@gmail.com>
2021-01-02 22:24:11 +0700

    add reducer for error state, still in the repair stage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import './styles/App.scss';
 
 import Login from './pages/Login';
@@ -7,13 +7,11 @@ import Login from './pages/Login';
 class App extends Component {
   render() {
     return (
-      <Router>
         <div className="App">
           <Switch>
             <Route path="/admin/login" component={Login}/>
           </Switch>
         </div>
-      </Router>
     );
   }
 }
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
-import logo from '../../img/logo_v2.png'
+import React, { useState, useReducer, useEffect } from 'react';
+import logo from '../../img/logo_v2.png';
 
-import InputText from '../../component/Input'
-import Button from '../../component/Button'
-import style from './Login.module.scss'
+import InputText from '../../component/Input';
+import Button from '../../component/Button';
+import style from './Login.module.scss';
+
+import reducer from './reducer';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    // masih belum
+    const [err, dispatch] = useReducer(reducer, '')
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // login submit
+
+        if(!(form.email && form.password)){
+            dispatch({type: 'BLANK'});
+        } else {
+            dispatch({type: 'SUCCESS'});
+        }
+
+        if(!err) {
+            alert(JSON.stringify(form))
+        }
     }
+
+    
 
     return (
         <div className={style.container}>
@@ -21,12 +39,15 @@ const Login = () => {
                 <h2 className={style.title}>LOGIN</h2>
                 <form onSubmit={handleSubmit}>
                     <div style={{marginBottom: '15px'}}>
-                        <InputText type='text' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} styles='marginBottom: 15px'/>
+                        <InputText type='email' placeholder='Email' value={form.email} onChange={(e) => setForm({...form, email: e.target.value})}/>
                     </div>
                     <div>
-                        <InputText type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <InputText type='password' placeholder='Password' value={form.password} onChange={(e) => setForm({...form, password: e.target.value})}/>
                     </div>
                     <div style={{marginTop: '30px'}}>
+                        {err && 
+                            <p className={style.alert}>{err}</p>
+                        }
                         <Button type='submit' width='200px'>LOGIN</Button>
                     </div>
                 </form>
